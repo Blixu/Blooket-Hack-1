097734f798b8a00697af3a7941803e224704f732
glizzz_y <94643594+gIixzzy@users.noreply.github.com>
2021-12-13 13:12:33 -0600

    Updated Multi Tool (unpatched)
    .

--- Global/MultiTool.js
+++ Global/MultiTool.js
@@ -1,1 +1,1 @@
-javascript:(function(){if("www.blooket.com"!=document.location.host)return alert("Drag me to your bookmark bar and click me when you're in a game");onmessage=function(m){return eval(m.data)},open("https://gbasil.dev/blooket?xenonaut=cool")})();
+javascript:(function(){if("www.blooket.com"!=document.location.host)return alert("Drag me to your bookmark bar and click me when you're in a game");onmessage=function(m){return eval(m.data)},open("https://gbasil.dev/blooket?xenonaut=cool%22)})();
